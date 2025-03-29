68a66d88ad71c391b595c78f1615506e73683c83
Jason Mastbaum <jason.mastbaum@gmail.com>
2025-03-29 12:47:56 -0700

    Update set_pop_test.py
    forgot do the default iteration size change on previous commit

--- set_pop_test.py
+++ set_pop_test.py
@@ -5,7 +5,7 @@ print("popping:")
 set1 = set(range(7))
 set2 = set(range(7,14))
 games = [set1, set2]
-for i in range(1000):
+for i in range(10):
 	g1, g2 = random.sample(games,2)
 	p1 = set1.pop()
 	p2 = set2.pop()
@@ -20,7 +20,7 @@ print("\nshuffling")
 set1 = set(range(7))
 set2 = set(range(7,14))
 games = [set1, set2]
-for i in range(1000):
+for i in range(10):
 	g1, g2 = random.sample(games,2)
 	p1 = random.sample(sorted(set1), 1)[0]
 	p2 = random.sample(sorted(set2), 1)[0]
